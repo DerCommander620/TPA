7a62d5a41e4c09f967cce3b069897b1474aebca5
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-06-22 21:21:01 +0200

    Update Main.php

--- src/TPA/Main.php
+++ src/TPA/Main.php
@@ -1,17 +1,59 @@
 <?php
 namespace TPA;
 
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\SingletonTrait;
-use TPA\Commands\TPACommand;
 
 class Main extends PluginBase{
-    use SingletonTrait;
-    const use_ingame = "§cPlease use it In-Game";
 
-    public function onEnable(): void{
-        $this->getServer()->getCommandMap()->registerAll($this->getName(), $this [
-            new TPACommand()
-        ]);
-    }
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+        if($sender instanceof Player){
+            if(empty($args[0])){
+                $sender->sendMessage("§cPlease enter a Player name or type /tpa help!");
+            }else{
+                $player = $args([0])->getPlayerExact();
+                if(!$player){
+                    $sender->sendMessage("§cThis Player is not Online!");
+                }else{
+                    $player->sendMessage("§e" . $sender->getName() . " §ahas send you a TPA request!");
+                    $sender->sendMessage("§aYou have successfully send a TPA request to §e" . $player);
+                }
+            }
+            switch(strtolower($args[0])){
+                case "help":
+                    $sender->sendMessage("§aHelp §7| §eTPA \n§f- §c/tpa help \n§f- §c/tpa accept <playername> \n§f- §c/tpa decline <playername>");
+                    break;
+                case "accept":
+                    $player = $args([1]);
+                    if(empty($player)){
+                        $sender->sendMessage("§cPlease enter a PlayerName who you want to accept his tpa request!");
+                    }
+                    if(!$player){
+                        $sender->sendMessage("§c " . $player . "Is not Online!");
+                    }else{
+                        $player->sendMessage($sender->getName() . " §aSuccessfully accepted your TPA request");
+                        $sender->sendMessage("§aYou have sucessfully accepted §e" . $player . "´s TPA request!");
+                        $sender->teleport($player->getWorld(), $player->getPosition());
+                    }
+                    break;
+                case "decline":
+                    $player = $args([1]);
+                    if(empty($player)){
+                        $sender->sendMessage("Please enter the PlayerName that send you a tpa request you want to delete!");
+                    }
+                    if(!$player){
+                        $sender->sendMessage("§cSorry but this Player is not Online!");
+                    }else{
+                        $sender->sendMessage("§aYou successfully declined §b" . $player . "´s TPA request");
+                        $player->sendMessage("§b" . $sender->getName() . " §cDenied your TPA request");
+                    }
+                    break;            
+            }
+        }else{
+            $sender->sendMessage("§cPlease use it In-Game");
+        }
+    return true;    
+    }    
 }
